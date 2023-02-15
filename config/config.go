d6c42c4b8b8b79b4954943aaaf30cea141923630
Serhii Chernov <gres.cher@icloud.com>
2023-02-15 19:25:35 +0200

    Add function to set config defaults

--- config/config.go
+++ config/config.go
@@ -1,43 +1,99 @@
 package config
 
 import (
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
 )
 
+const (
+	envFilePath = "config.env"
+
+	serverAddress = "SERVER_ADDR"
+	serverPort    = "SERVER_PORT"
+	dbAddress     = "DB_ADDR"
+	dbPort        = "DB_PORT"
+	dbName        = "DB_NAME"
+	dbUser        = "DB_USER"
+	dbPassword    = "DB_PASS"
+)
+
 func init() {
-	err := godotenv.Load("config.env")
+	err := godotenv.Load(envFilePath)
 	if err != nil {
-		log.Fatalf("Config error: %v", err)
+		log.Println(err)
+		setDefaults()
+		if err = writeConfig(envFilePath); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
 
 func ServerAddress() string {
-	return os.Getenv("SERVER_ADDR")
+	return os.Getenv(serverAddress)
 }
 
 func ServerPort() string {
-	return os.Getenv("SERVER_PORT")
+	return os.Getenv(serverPort)
 }
 
-func DBAddr() string {
-	return os.Getenv("DB_ADDR")
+func DBAddress() string {
+	return os.Getenv(dbAddress)
 }
 
 func DBPort() string {
-	return os.Getenv("DB_PORT")
+	return os.Getenv(dbPort)
 }
 
 func DBName() string {
-	return os.Getenv("DB_NAME")
+	return os.Getenv(dbName)
 }
 
 func DBUser() string {
-	return os.Getenv("DB_USER")
+	return os.Getenv(dbUser)
 }
 
 func DBPassword() string {
-	return os.Getenv("DB_PASS")
+	return os.Getenv(dbPassword)
+}
+
+func setDefaults() {
+	log.Println("setting defaults")
+	os.Setenv(serverAddress, "127.0.0.1")
+	os.Setenv(serverPort, "8080")
+	os.Setenv(dbAddress, "127.0.0.1")
+	os.Setenv(dbPort, "5432")
+	os.Setenv(dbName, "payments")
+	os.Setenv(dbUser, "payments")
+	os.Setenv(dbPassword, "lth gfhjk")
+}
+
+func writeConfig(path string) error {
+	log.Println("creating config.env file")
+	params := os.O_WRONLY | os.O_CREATE | os.O_APPEND
+	file, err := os.OpenFile(path, params, 0600)
+	if err != nil {
+		return err
+	}
+
+	log.Println("saving defaults")
+	_, err = fmt.Fprint(
+		file,
+		serverAddress+":"+ServerAddress()+"\n",
+		serverPort+":"+ServerPort()+"\n",
+		dbAddress+":"+DBAddress()+"\n",
+		dbPort+":"+DBPort()+"\n",
+		dbName+":"+DBName()+"\n",
+		dbUser+":"+DBUser()+"\n",
+		dbPassword+":"+DBPassword()+"\n",
+	)
+	if err != nil {
+		return err
+	}
+	if err = file.Close(); err != nil {
+		return err
+	}
+	return nil
 }
